1d2146dfeffad36586934bf50e3812dea2613e76
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-15 14:24:20 -0700

    Write a program that prints all single digit numbers of base 10 starting from

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print single digit number base ten
+ * Return: 0 success
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d <= 57)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
