bc9b363222924045981d1f9e0658591c8d014323
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-17 08:13:09 -0700

    Task eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - Write a C program that prints exactly
-*Return: 1 success
+* main - Write a C program that prints aline to the standard error
+* Return: 1 success
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
