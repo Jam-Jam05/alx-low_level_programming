9d02f3f3a6c5a5e2c5689e4b55706150f253091b
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-17 09:25:48 -0700

    Task eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include  <unistd.h>
+
 /**
 * main - Write a C program that prints aline to the standard error
 * Return: 1 success
 */
 int main(void)
 {
+	char errorMessage[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n";
 
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, &errorMessage, sizeof(errorMessage) - 1);
+	return (1);
 }
