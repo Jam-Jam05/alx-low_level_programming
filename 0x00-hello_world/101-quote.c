c7b7f66b58e814870dcee523a606396bc1854f4a
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-17 03:00:54 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - A c program that prints a lineto the standard error
- *
- * Return: Always 1 (Success)
- */
+*main - Write a C program that prints exactly
+*Return: 1 success
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,"
+			"2015-10-19\n");
 	return (1);
 }
