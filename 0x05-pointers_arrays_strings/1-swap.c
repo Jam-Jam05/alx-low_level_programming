5579ebec7a785e26a0f8000ca9fcd2b627c8f166
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-24 15:19:22 -0700

    Task one

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,8 @@
-#include "main.h"
 /**
 *swap_int - function to swap to vars
 *
 *@a: the inputed var 1
 *@b: the inputed var 2
-*
-*Return: return 1 if it an upper case and 0 otherwise
 */
 void swap_int(int *a, int *b)
 {
