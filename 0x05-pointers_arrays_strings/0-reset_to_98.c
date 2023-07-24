c25a1b3204e9fa9b951126711db4daa256f38053
Jam-Jam05 <alabi.j.adekunle@gmail.com>
2023-07-24 15:00:42 -0700

    Tak zero

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
-#include "main.h"
 /**
 *reset_to_98 - function to reset a var to 98
 *
 *@n: the inputed reference to a var
 *
-*Return:void
 */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strlen - function to get the len of a string
+*
+*@s: the inputed pointer of array
+*
+*Return: return the length of the array
+*/
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*(s + a) != 0)
+	{
+	a++;
+	}
+	return (a);
+}
